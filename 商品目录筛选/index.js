425b063f8721bd0f6a931642ee066331feda1245
Gravity <1987391831@qq.com>
2020-06-14 09:26:10 +0800

    finish

--- 商品目录筛选/index.js
+++ 商品目录筛选/index.js
@@ -1,6 +1,7 @@
 var dls = document.querySelectorAll('dl:not(.select)');
 var selected = document.querySelector('.select');
 
+
 for (var i = 0; i < dls.length; i++) {
     //mark属性，false不用创建标签
     dls[i].mark = false;
@@ -14,13 +15,18 @@ function select(n) {
     var dds = dls[n].querySelectorAll('dd');
     var dd = null;
     var prev = null; //上一次选中的一个标签
+    var span = null;
+    var num = 0;
     for (var i = 0; i < dds.length; i++) {
         // prev && (prev.className = '');
         if (prev) {
             prev.className = '';
         }
         dds[i].onclick = function () {
-            prev.className = '';
+            if (num != 0) {
+                prev.className = '';
+            }
+            num++;
             this.className = 'active';
             prev = this;
 
@@ -29,18 +35,17 @@ function select(n) {
                 dd = document.createElement('dd');
                 dd.innerHTML = this.innerHTML;
                 selected.appendChild(dd);
-
                 parent.mark = true;
-            }
 
-            //         // var This = this;
-            var span = document.createElement('span');
+            } else {
+                dd.innerHTML = this.innerHTML;
+            }
+            span = document.createElement('span');
             span.innerHTML = '×';
             span.onclick = function () {
                 selected.removeChild(dd);
                 //下次点击能够在创建标签
                 parent.mark = false;
-                This.className = '';
                 prev.className = '';
             }
             dd.appendChild(span);
